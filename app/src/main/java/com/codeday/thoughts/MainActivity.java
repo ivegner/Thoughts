59570763b45e9fa2d91bbd9b970fd6853abbe8bc
dl-eric <orangepamelo@gmail.com>
2015-11-07 18:52:57 -0500

    Things.

--- app/src/main/java/com/codeday/thoughts/MainActivity.java
+++ app/src/main/java/com/codeday/thoughts/MainActivity.java
@@ -7,12 +7,17 @@ import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isParseInitialized = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        Parse.initialize(this, "amWtu0dCB9xx0XJ8a4dQA4cy7XayDikxCGpdhkVb", "0XbZemPscEidvvl1NE0wrCKXQayuS48Fr7I6XF4O");
+        if(!isParseInitialized) {
+            Parse.initialize(this, "amWtu0dCB9xx0XJ8a4dQA4cy7XayDikxCGpdhkVb", "0XbZemPscEidvvl1NE0wrCKXQayuS48Fr7I6XF4O");
+            isParseInitialized = true;
+        }
     }
 }
