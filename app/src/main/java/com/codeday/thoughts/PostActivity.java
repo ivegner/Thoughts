c35fab8e18636d5f5ce1e50ef761b30e2008a7e5
dl-eric <orangepamelo@gmail.com>
2015-11-07 22:51:32 -0500

    Delete POST button, add Send button in ActionBar

--- app/src/main/java/com/codeday/thoughts/PostActivity.java
+++ app/src/main/java/com/codeday/thoughts/PostActivity.java
@@ -3,6 +3,7 @@ package com.codeday.thoughts;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -20,6 +21,13 @@ public class PostActivity extends AppCompatActivity {
         inputLength = 600;
         TextView charRemain = (TextView) findViewById(R.id.charRemainView);
         charRemain.setText(inputLength + " characters remaining.");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_post; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_post, menu);
+        return true;
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
