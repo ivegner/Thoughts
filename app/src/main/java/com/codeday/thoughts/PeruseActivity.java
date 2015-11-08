867597eea7cde83579626b71001ecbd74eaeb30e
Da-Jin Chu <dajinchu@gmail.com>
2015-11-07 23:43:43 -0500

    Swipe gesture detect

--- app/src/main/java/com/codeday/thoughts/PeruseActivity.java
+++ app/src/main/java/com/codeday/thoughts/PeruseActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class PeruseActivity extends AppCompatActivity {
+
+    private TextView textViewMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peruse);
+
+        textViewMessage = (TextView) findViewById(R.id.viewTextMessage);
+        findViewById(R.id.messageContainer).setOnTouchListener(new OnSwipeTouchListener(this){
+            @Override
+            public void onSwipeLeft() {
+                textViewMessage.setText("hey");
+            }
+        });
+
     }
 
     public void startPostActivity(View v){
