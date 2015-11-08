aa2d68417d99088fd5c14c55e60ca77629126fae
dl-eric <orangepamelo@gmail.com>
2015-11-07 22:37:45 -0500

    Implemented Basic Post UI

--- app/src/main/java/com/codeday/thoughts/PostActivity.java
+++ app/src/main/java/com/codeday/thoughts/PostActivity.java
@@ -1,7 +1,9 @@
 package com.codeday.thoughts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,6 +20,13 @@ public class PostActivity extends AppCompatActivity {
         inputLength = 600;
         TextView charRemain = (TextView) findViewById(R.id.charRemainView);
         charRemain.setText(inputLength + " characters remaining.");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), PeruseActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
     }
 
     public void post(View view) {
