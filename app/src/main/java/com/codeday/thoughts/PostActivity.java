ba46eb2aab6861b070f3d3b00ad753ec77e54422
Da-Jin Chu <dajinchu@gmail.com>
2015-11-08 07:40:22 -0500

    Chars remaining updates

--- app/src/main/java/com/codeday/thoughts/PostActivity.java
+++ app/src/main/java/com/codeday/thoughts/PostActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -54,8 +56,19 @@ public class PostActivity extends AppCompatActivity {
 
         inputLength = 600;
         charRemainView = (TextView) findViewById(R.id.charRemainView);
-        inputLength = 600 - inputView.getText().toString().length();
-        charRemainView.setText(inputLength + " characters remaining.");
+        inputView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                inputLength = 600 - inputView.getText().toString().length();
+                charRemainView.setText(inputLength + " characters remaining.");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     public void takePictureWithIntent(View v){
