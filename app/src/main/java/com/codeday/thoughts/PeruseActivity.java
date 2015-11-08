c1840f8a4f45acb5f56c3ffb6667260b1dfae396
Da-Jin Chu <dajinchu@gmail.com>
2015-11-07 21:47:33 -0500

    PeruseActivity opens PostActivity through FAB

--- app/src/main/java/com/codeday/thoughts/PeruseActivity.java
+++ app/src/main/java/com/codeday/thoughts/PeruseActivity.java
@@ -1,7 +1,9 @@
 package com.codeday.thoughts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class PeruseActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PeruseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peruse);
+    }
+
+    public void startPostActivity(View v){
+        Intent intent = new Intent(this,PostActivity.class);
+        startActivity(intent);
     }
 }
