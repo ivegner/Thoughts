8c80e84c3c870da0d8d7cb5512129f495b15e016
dl-eric <orangepamelo@gmail.com>
2015-11-08 04:56:18 -0500

    Add card + database pull functionality!

--- app/src/main/java/com/codeday/thoughts/PeruseActivity.java
+++ app/src/main/java/com/codeday/thoughts/PeruseActivity.java
@@ -1,5 +1,6 @@
 package com.codeday.thoughts;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,40 +8,53 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PeruseActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    String[] placeholder = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"};
+    private Context context;
+
+    String[] placeholder = {};
     ArrayList<String> myDataset = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peruse);
+        context = this;
 
         myDataset.addAll(Arrays.asList(placeholder));
 
         mRecyclerView = (RecyclerView) findViewById(R.id.peruse_recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(context);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
 
-        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
                 return false;
             }
+
             @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir){
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 int index = viewHolder.getLayoutPosition();
                 myDataset.remove(index);
                 mAdapter.notifyItemRemoved(index);
@@ -49,6 +63,8 @@ public class PeruseActivity extends AppCompatActivity {
 
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         itemTouchHelper.attachToRecyclerView(mRecyclerView);
+
+        parseQuery();
     }
 
     @Override
@@ -56,8 +72,23 @@ public class PeruseActivity extends AppCompatActivity {
         //Nothing!
     }
 
-    public void startPostActivity(View v){
-        Intent intent = new Intent(this,PostActivity.class);
+    public void startPostActivity(View v) {
+        Intent intent = new Intent(this, PostActivity.class);
         startActivity(intent);
+    }
+
+    private void parseQuery() {
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Content");
+        query.whereExists("Text");
+        try {
+            List<ParseObject> parseObjects = query.find();
+            for(int i = 0; i < parseObjects.size(); i++) {
+                myDataset.add(parseObjects.get(i).getString("Text"));
+                mAdapter.notifyDataSetChanged();
+            }
+        } catch (ParseException e ) {
+            e.printStackTrace();
+        }
     }
 }
